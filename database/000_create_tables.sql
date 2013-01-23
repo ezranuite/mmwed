96bbf6b5b52ad859123e5365ad26dab8cdbbff5a
Ezra Nuite <enuite@gmail.com>
2013-01-23 19:49:26 +0000

    Fix sql syntax and add password field

--- database/000_create_tables.sql
+++ database/000_create_tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    password VARCHAR(1000) NOT NULL,
     phone VARCHAR(15)
 );
 
@@ -13,12 +14,12 @@ CREATE TABLE beers (
     style VARCHAR(100),
     country VARCHAR(100),
     state VARCHAR(2)
-)
+);
 
 CREATE TABLE food (
    food_id BIGSERIAL,
    name VARCHAR(250) NOT NULL
-)
+);
 
 CREATE TABLE movies (
     movie_id BIGSERIAL,
@@ -27,13 +28,13 @@ CREATE TABLE movies (
     brought_by_user_id BIGINT NOT NULL,
     release_date DATE,
     imdb_url VARCHAR(2500)
-)
+);
 
 CREATE TABLE meetings (
    meeting_id BIGSERIAL,
    date DATE NOT NULL,
    movie_id BIGINT
-)
+);
 
 CREATE TABLE attendance (
    attendance_id BIGSERIAL,
@@ -44,4 +45,4 @@ CREATE TABLE attendance (
    food_id BIGINT,
    movie_id BIGINT,
    expense INTEGER
-)  
+);  
